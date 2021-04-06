f3219a44b4f35f6e677ce0ac08bc6fd66e2b5653
riccardopizzoni <riccardo@luckyseven.it>
2021-04-06 19:57:15 +0200

    Login Page First Setup

--- lib/home.dart
+++ lib/home.dart
@@ -9,10 +9,13 @@ class Home extends StatefulWidget {
 
 class HomeState extends State<Home> {
   int counter = 0;
+  bool signupButtonVisible = true;
+  String textFieldString = "";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.yellow.shade200,
       appBar: AppBar(
         title: Text("Social Hackathon"),
       ),
@@ -20,12 +23,68 @@ class HomeState extends State<Home> {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Text("Contatore"),
-            Text("$counter"),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16),
+              child: Text("Contatore"),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("$counter"),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(textFieldString),
+            ),
+            Container(
+              padding: EdgeInsets.all(16),
+              width: 240,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Email",
+                  labelText: "Username",
+                  //errorText: "Inserisci email",
+                ),
+                onChanged: (text) {
+                  setState(() {
+                    textFieldString = text;
+                  });
+                },
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                color: Colors.white,
+                border: Border.all(color: Colors.red),
+              ),
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Visibility(
+                  visible: signupButtonVisible,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text("SignUp"),
+                  ),
+                ),
+                SizedBox(
+                  width: 32,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    //TODO: implementare pulsante login
+                    setState(() {
+                      signupButtonVisible = true;
+                    });
+                  },
+                  child: Text("Login"),
+                ),
+              ],
+            ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
         onPressed: () {
           setState(() {
             counter++;
@@ -34,5 +93,4 @@ class HomeState extends State<Home> {
       ),
     );
   }
-
 }
